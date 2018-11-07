9bdecc0a2ba4cd838d98c42c94d8e1be04fcdec8
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2018-11-07 19:05:50 +0000

    make more general

--- src/clean_raw_data.py
+++ src/clean_raw_data.py
@@ -1,7 +1,13 @@
 import glob
-import pandas as pd
 import json
+
+import pandas as pd
+
 import arcas
+import unidecode
+
+path = 'raw_data/'
+topic = 'pd'
 
 def normalise_names(s):
     elements = s.split()
@@ -16,29 +22,30 @@ for api in [arcas.Nature(), arcas.Ieee(), arcas.Plos(), arcas.Arxiv(), arcas.Spr
     raw_articles = []
     api_name = api.__class__.__name__
 
-    for filename in glob.glob('raw_data/*_{}_*.json'.format(api_name)):
+    for filename in glob.glob('{}*_{}_*.json'.format(path, api_name)):
         with open(filename) as json_data:
             d = json.load(json_data)
             raw_articles.append(d)
+    if raw_articles:
+        flat_list = [item for sublist in raw_articles for item in sublist]
 
-    flat_list = [item for sublist in raw_articles for item in sublist]
+        articles = []
+        for art in flat_list:
+            articles.append(api.to_dataframe(art))
 
-    articles = []
-    for art in flat_list:
-        articles.append(api.to_dataframe(art))
+        dataframe = pd.concat(articles, ignore_index=True)
+        dataframe = dataframe[~(dataframe['author']=='No authors found for this document.')]
+        names = dataframe.author
 
-    dataframe = pd.concat(articles, ignore_index=True)
-    dataframe = dataframe[~(dataframe['author']=='No authors found for this document.')]
-    names = dataframe.author
+        if api_name == 'Springer':
+            edited = []
+            for name in names:
+                first, last = name.split(' ', 1)
+                edited.append(last + ' ' + first)
+            names = edited
 
-    if api_name == 'Springer':
-        edited = []
-        for name in names:
-            first, last = name.split(' ', 1)
-            edited.append(first + ' ' + last)
-        names = edited
+        edited_names = [normalise_names(name) for name in names]
+        unicoded_names = [unidecode.unidecode(name) for name in edited_names]
+        dataframe.author =  unicoded_names
 
-    edited_names = [normalise_names(name) for name in names]
-    dataframe.author = edited_names
-
-    api.export(dataframe, 'data/{}.json'.format(api_name))
+        api.export(dataframe, 'data/{}_{}.json'.format(topic, api_name))
