189ff1e0b18e5f41f3e81e1c96ef83a9b877f380
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2018-11-26 12:25:02 +0000

    add dictionary for periods and corresponind years

--- src/tools.py
+++ src/tools.py
@@ -1,7 +1,9 @@
+from scipy import stats
+
 def normalise_names(s):
     elements = s.split()
     
-    new = elements[0][0].upper()+'.'
+    new = elements[0].title() + ' ' #[0].upper()+'.'
     new += elements[-1].title()
     
     return new
@@ -10,3 +12,12 @@ def write_to_file(filename, metric):
     file = open("../../assets/{}".format(filename), 'w')
     file.write('{}'.format(metric))
     file.close()
+
+def test_kruskal(distributions):
+    alpha = 0.05
+    _, p = stats.kruskal(distributions[0],distributions[1], distributions[2])
+    
+    if p < alpha:
+        print(p, "The null hypothesis can be rejected.")
+    else:
+        print(p, "The null hypothesis cannot be rejected.")
